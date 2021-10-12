5d9b1674372534d30ea70822ac77f52241951b4b
Kévin DOQUERO <kevin.doquero@contentsquare.com>
2021-10-12 16:39:28 +0200

    chore: added composition api

---
+++ src/components/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+      <div>
+          <h1>Hello I'm Niska</h1>
+          
+      </div>
+  </section>
+</template>
+
+<script>
+import { defineComponent, ref, toRefs, PropType } from '@vue/composition-api';
+export default defineComponent({
+  props: {
+  },
+  setup(props) {
+    
+
+    return {
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
