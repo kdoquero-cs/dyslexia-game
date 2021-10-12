4c8b549b0c0d612aac0c3e412db8d887dfd2a051
Kévin DOQUERO <kevin.doquero@contentsquare.com>
2021-10-12 23:04:58 +0200

    add csmat

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,54 +5,56 @@
       <form action="">
         <div class="form">
           <label for="name">What's your {{ title }}</label>
-          <input
-            v-if="!showAge"
-            v-model="name"
-            :ref="name"
-            title="name"
-            type="text"
-          />
+          <csm-input type="text" @csmInput="setName"></csm-input>
           <div v-if="showAge">
-            <label for="age"></label>
-            <input title="years" type="text" /><input
-              title="months"
-              type="text"
-            />
+            <label for="years">Years</label>
+            <csm-input name="years" @csmInput="setInput"></csm-input>
+            <input v-model="name" title="years" type="text" />
+            <label for="months">Months</label>
+            <input title="months" type="text" />
           </div>
           <p>{{ name }}</p>
         </div>
       </form>
       <div>
-          <button @click="toogleShowAge">Next</button>
+        <button @click="toogleShowAge">Next</button>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import { computed, defineComponent, ref} from "@vue/composition-api";
+import { computed, defineComponent, ref } from "@vue/composition-api";
+import router from "@/router";
 export default defineComponent({
   props: {},
   setup() {
     const name = ref("");
+    const years = ref("");
+    const months = ref("");
+
     const showAge = ref(false);
     const title = computed(() => (showAge.value ? "birthday" : "name"));
-    
-    const  goToPlay = () => {
-     
-    }
-    const toogleShowAge = () => {
-      showAge.value = !showAge.value;
-      if (showAge.value) {
-          goToPlay();
-      }
+    const setName = (event) => {
+      name.value = event.detail.value;
     };
-    
+    const setInput = (event) => {};
+    const toogleShowAge = () => {
+      if (showAge.value) {
+        router.push({ path: "/start" });
+      }
+      showAge.value = !showAge.value;
+    };
+
     return {
       showAge,
       name,
       toogleShowAge,
       title,
+      years,
+      months,
+      setInput,
+      setName,
     };
   },
 });
@@ -61,11 +63,11 @@ export default defineComponent({
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 input {
-    margin: 5px;
+  margin: 5px;
 }
 </style>
