7013a6456ce9dfa44bbdd7363807d10baa0d76ea
JMDidon <jeanmichel.didon@gmail.com>
2021-10-13 17:06:46 +0200

    Choose companion page done

--- src/components/CompagnionChoice.vue
+++ src/components/CompagnionChoice.vue
@@ -1,32 +1,34 @@
 <template>
-  <div>
-    <fieldset class="fieldset">
+  <div class="container">
+    <fieldset class="main-form">
       <legend>
-        <h1 class="h1">Before anything, choose your companion</h1>
+        <h1 class="main-title">Before anything, choose your companion</h1>
       </legend>
-      <!-- Focus should be visible on the image, not the radio button -->
-      
-      <label for="PhoenixInput">
-        <input id="PhoenixInput" name="avatar" type="radio" class="sr-only" />
-        <img src="@/assets/avatars/Animal1.svg" alt="Phoenix" class="RadioImg" />
-      </label>
-     
-      <label for="UnicornInput">
-         <input id="UnicornInput" name="avatar" type="radio" class="sr-only" />
-        <img src="@/assets/avatars/Animal2.svg" alt="Unicorn" class="RadioImg" />
-      </label>
-     
-      <label for="FoxInput">
-         <input id="FoxInput" name="avatar" type="radio" class="sr-only" />
-        <img src="@/assets/avatars/Animal3.svg" alt="Fox" class="" />
-      </label>
-     
-      <label for="DragonInput">
-         <input id="DragonInput" name="avatar" type="radio" class="sr-only" />
-        <img src="@/assets/avatars/Animal4.svg" alt="Dragon" class="RadioImg" />
-      </label>
+
+      <div class="companion-wrapper">
+        <label for="PhoenixInput" class="companion-btn">
+          <input id="PhoenixInput" name="avatar" type="radio" class="radio sr-only" />
+          <img src="@/assets/avatars/Animal1.svg" alt="Phoenix" class="companion-img" />
+        </label>
+
+        <label for="UnicornInput" class="companion-btn">
+          <input id="UnicornInput" name="avatar" type="radio" class="radio sr-only" />
+          <img src="@/assets/avatars/Animal2.svg" alt="Unicorn" class="companion-img" />
+        </label>
+
+        <label for="FoxInput" class="companion-btn">
+          <input id="FoxInput" name="avatar" type="radio" class="radio sr-only" />
+          <img src="@/assets/avatars/Animal3.svg" alt="Fox" class="companion-img" />
+        </label>
+
+        <label for="DragonInput" class="companion-btn">
+          <input id="DragonInput" name="avatar" type="radio" class="radio sr-only" />
+          <img src="@/assets/avatars/Animal4.svg" alt="Dragon" class="companion-img" />
+        </label>
+      </div>
+
+      <csm-button class="next" @csmClick="goToForm">Next</csm-button>
     </fieldset>
-     <csm-button @csmClick="goToForm">Next</csm-button>
   </div>
 </template>
 
@@ -48,49 +50,78 @@ export default {
 };
 </script>
 
-<style>
-.fieldset {
-  border-color: #ECECEC;
-  border-width: 2px;
-  border-radius: 30px;
-  padding: 30px;
-  background-color: #FFFFFF;
+<style scoped>
+.container {
+  height: 100%;
+  padding: 5% 10%;
 }
-.h1 {
-  font-size: 24px;
-  font-weight: bold;
-  color: #3C3D40;
+
+.main-form {
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+  align-self: center;
+  height: 100%;
+  border: none;
+  padding: 0;
+  margin: 0;
 }
+
+.main-title {
+  margin: 0;
+  margin-bottom: 1em;
+  padding: var(--spacing-sp-3);
+  font-size: 2em;
+  border-radius: 16px;
+  background-color: var(--colors-gray-lighter-40);
+}
+
 .button {
   border-color: #ECECEC;
-  border-width: 2 px;
-  padding: 12 px;
+  border-width: 2px;
+  padding: 12px;
   background-color: #BCCF00;
   font-size: 15px;
-  font-weight: semi-bold;
+  font-weight: 600;
   color: #3C3D40;
 }
-.RadioImg {
-  border-width: 0px;
-  min-height:0;
-  min-width:0 ;
-}
-img {
-  width: 100px;
-  margin: 0 15px;
-  cursor: pointer;
-}
-img:focus {
-    outline: 2px solid blue;
-}
-/* IMAGE STYLES */
-[type=radio] + img {
-  cursor: pointer;
+
+.companion-wrapper {
+  flex: 1;
+  display: flex;
+  flex-flow: row nowrap;
 }
 
-/* CHECKED STYLES */
-[type=radio]:checked + img {
-  outline: 2px solid #f00;
+.companion-btn {
+  display: flex;
+  flex: 1;
+  width: 25%;
+  align-items: center;
 }
 
+.companion-img {
+  margin: 0;
+  height: 50%;
+  min-height: 0;
+  min-width: 0;
+  cursor: pointer;
+  padding: 2em;
+  border: 2px solid transparent;
+  border-radius: var(--border-radius-br-16);
+  transition: transform .2s ease-in-out, border-color .2s ease-in-out;
+}
+
+.companion-img:hover {
+  transform: scale(1.1);
+}
+
+.radio:focus + .companion-img,
+.radio:checked + .companion-img {
+  transform: scale(1.1);
+  border-color: var(--colors-gray-lighter-40);
+}
+
+.next {
+  margin-top: 2em;
+}
 </style>
